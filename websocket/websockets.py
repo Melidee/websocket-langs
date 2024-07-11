6b67d50028e1f3235f0b691bc914cfc535795557
Amelia Rossi <w.ardecan@gmail.com>
2024-07-11 10:55:14 -0500

    rename module to websocket

--- websocket/websockets.py
+++ websocket/websockets.py
@@ -1,21 +1,38 @@
 import socket, hashlib
 from base64 import b64encode
 from typing import Optional
-from websockets.http import Request, Response
+from websocket.http import Request, Response
 
 class Websocket:
-    def __init__(self) -> None:
+    def __init__(self, conn: socket.socket, is_server: bool) -> None:
         pass
+        
+    @staticmethod
+    def connect(addr: tuple[str, int]) -> "Websocket" | None:
+        conn = socket.create_connection(addr, 3)
+        req = Request("GET", "/", headers={
+            "Upgrade": "websocket",
+            "Connection": "Upgrade",
+            "Sec-Websocket-Key": "",
+            "Sec-Websocket-Version": "13",
+        })
+        conn.sendall(str(req))
+        data = conn.recv(2048)
+        if not data:
+            return None
+        res = Response.parse(data)
 
     @staticmethod
     def from_server() -> "Websocket":
         pass
 
 class WebsocketServer:
-    def __init__(self, addr: tuple[str, int]) -> None:
+    def __init__(self, addr: tuple[str, int] = ('', 8080)) -> None:
+        self.connections: list[socket.socket] = []
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind(addr)
+        self.sock.create_serve
         self.sock.listen()
+        
 
     def accept(self) -> Optional[Websocket]:
         conn, _addr = self.sock.accept()
@@ -30,12 +47,12 @@ class WebsocketServer:
             or (ws_key := req.headers.get("Sec-Websocket-Key")) == None:
             conn.close()
             return None
-        res = str(Response(status="101 Switching Protocols", headers={
+        res = Response(status="101 Switching Protocols", headers={
             "Upgrade": "websocket",
             "Connection": "Upgrade",
             "Sec-Websocket-Accept": make_sec_ws_accept(ws_key),
-        })).encode("utf-8")
-        conn.sendall(res)
+        })
+        conn.sendall(str(res).encode('utf-8'))
         return Websocket.from_server()
 
     def __enter__(self):
@@ -45,6 +62,8 @@ class WebsocketServer:
         self.close()
 
     def close(self):
+        for conn in self.connections:
+            conn.close()
         self.sock.close()
 
 WS_MAGIC_WORD = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
