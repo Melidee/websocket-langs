0dd59c4e4c2e7f3cf04a0c13b97b21864b93f75f
Amelia Rossi <w.ardecan@gmail.com>
2024-07-15 03:24:25 -0500

    fix typing

--- websocket/http.py
+++ websocket/http.py
@@ -1,6 +1,6 @@
 import hashlib, os
-from base64 import b64encode
-from typing import Self
+from base64 import b64encode, b64decode
+from typing import Self, Optional
 
 HEADER_WS_VERSION = "Sec-WebSocket-Version"
 HEADER_WS_KEY = "Sec-WebSocket-Key"
@@ -8,13 +8,14 @@ HEADER_WS_ACCEPT = "Sec-WebSocket-Accept"
 HEADER_WS_PROTOCOL = "Sec-WebSocket-Protocol"
 HEADER_WS_EXTENSIONS = "Sec-WebSocket-Extensions"
 
+
 class Request:
     def __init__(
         self,
         method: str = "GET",
         url: str = "/",
         headers: dict[str, str] = {},
-        body: str = "",
+        body: str = ""
     ) -> None:
         self.method: str = method
         self.url: str = url
@@ -23,26 +24,26 @@ class Request:
         self._set_body(body)
 
     @staticmethod
-    def parse(raw_request: str) -> Self:
+    def parse(raw_request: str) -> "Request":
         lines = raw_request.split("\r\n")
         try:
-            lines, body = lines[: lines.index("")], lines[lines.index("") + 1 :]
+            lines, body_splits = lines[: lines.index("")], lines[lines.index("") + 1 :]
         except ValueError:
             raise ValueError("No trailing line after headers")
-        body = "\r\n".join(body)
+        body = "\r\n".join(body_splits)
         try:
             method, url, proto = lines[0].split(" ")
-        except ValueError or IndexError:
+        except (ValueError, IndexError):
             raise ValueError(f"Invalid first line of request: {lines[0]}")
-        if proto != "HTTP/1.1" or proto != "HTTP/1.0":
+        if proto != "HTTP/1.1" and proto != "HTTP/1.0":
             raise ValueError("Invalid HTTP protocol specified by request")
         headers = {}
         for header_pair in lines[1:]:
             try:
-                key, val = header_pair.split(": ", 1)
+                key, val = header_pair.split(": ")
+                headers[key] = val
             except ValueError:
-                ValueError(f"Invalid Header format: {header_pair}")
-            headers[key] = val
+                raise ValueError(f"Invalid Header format: {header_pair}")
         return Request(method, url, headers, body)
 
     @staticmethod
@@ -59,7 +60,7 @@ class Request:
             },
         )
 
-    def is_valid_ws_request(self):
+    def is_valid_ws(self):
         return (
             self.method == "GET"
             and self.headers.get("Host") != None
@@ -83,17 +84,20 @@ class Request:
 
     def __str__(self) -> str:
         return f"{self.method} {self.url} HTTP/1.1\r\n{format_headers(self.headers)}\r\n{self._body}"
+    
+    def __bytes__(self) -> bytes:
+        return str(self).encode('utf-8')
 
     def __eq__(self, other: object) -> bool:
-        try:
-            return (
-                self.method == other.method
-                and self.url == other.url
-                and self.headers == other.headers
-                and self.body == other.body
-            )
-        except AttributeError:
+        if not isinstance(other, Request):
             return False
+        return (
+            self.method == other.method
+            and self.url == other.url
+            and self.headers == other.headers
+            and self.body == other.body
+        )
+
 
 
 class Response:
@@ -105,18 +109,22 @@ class Response:
         self._body = body
 
     @staticmethod
-    def parse(raw_response: str) -> Self | None:
+    def parse(raw_response: str) -> Optional["Response"]:
+        lines = raw_response.split("\r\n")
+        lines, body_splits = lines[: lines.index("")], lines[lines.index("") + 1 :]
+        body = '\r\n'.join(body_splits)
         try:
-            lines = raw_response.split("\r\n")
-            lines, body = lines[: lines.index("")], lines[lines.index("") + 1 :]
-            [_, status] = lines[0].split(" ")
-            headers = {}
-            for header_pair in lines[1:]:
+            _proto, status = lines[0].split(" ", 1)
+        except (ValueError, IndexError):
+            raise ValueError(f"Invalid first line of response: {lines[0]}")
+        headers = {}
+        for header_pair in lines[1:]:
+            try:
                 (key, val) = header_pair.split(": ")
-                headers[key] = val
-            return Response(status, headers, body)
-        except Exception:
-            return None
+            except ValueError:
+                raise ValueError(f"Invalid Header format: {header_pair}")
+            headers[key] = val
+        return Response(status, headers, body)
 
     @staticmethod
     def new_ws(ws_key: str):
@@ -142,7 +150,7 @@ class Response:
 
     def _set_body(self, body) -> None:
         self._body = body
-        self.headers["Content-Length"] = len(bytes(body))
+        self.headers["Content-Length"] = str(len(bytes(body)))
 
     def _del_body(self) -> None:
         del self._body
@@ -155,14 +163,14 @@ class Response:
         )
 
     def __eq__(self, other: object) -> bool:
-        try:
-            return (
-                self.status == other.status
-                and self.headers == other.headers
-                and self.body == other.body
-            )
-        except AttributeError:
+        if not isinstance(other, Response):
             return False
+        return (
+            self.status == other.status
+            and self.headers == other.headers
+            and self.body == other.body
+        )
+
 
 
 def format_headers(headers: dict[str, str]) -> str:
@@ -175,8 +183,8 @@ WS_MAGIC_WORD = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
 def make_sec_ws_accept(ws_key: str) -> str:
     digest = hashlib.sha1((ws_key + WS_MAGIC_WORD).encode("utf-8"))
     hash = digest.digest()
-    return b64encode(hash)
+    return b64encode(hash).decode('utf-8')
 
 
 def new_sec_ws_key() -> str:
-    return b64encode(os.urandom(16))
+    return b64encode(os.urandom(16)).decode('utf-8')
